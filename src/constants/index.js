657a0cbcab6004f2e17670076696e1a05f893905
Brian Jacobel <bjacobel@gmail.com>
2016-10-07 10:07:14 -0400

    Should handle both incremental and full updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
 export const SHOW_DEV_TOOLS = !process.env.production;
-export const FIREBASE_URL = 'https://covit-ddae8.firebaseio.com/';
+export const FIREBASE_URL = 'https://covit-test-jz.firebaseio.com/';
