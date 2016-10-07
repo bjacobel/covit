2a7619dd73fe160300f454c5b710091699a938c6
Brian Jacobel <bjacobel@gmail.com>
2016-10-07 13:32:57 -0400

    Fix avatars sorting, with placeholder API

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,30 +2,42 @@ import React, { Component } from 'react';
 import '../stylesheets/avatars.css';
 
 export default class Avatar extends Component {
-
   componentWillMount() {
-    this.state = {avatarUrl: 'http://placehold.it/32x32'};
-  }
-
-  componentDidMount() {
     let { username } = this.props;
 
     fetch(`https://api.github.com/users/${username}`)
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error('probably rate limited');
+        }
+      })
+      .then((json) => {
         this.setState({avatarUrl: json['avatar_url'] + '&size=32'});
-      });
+      })
+      .catch(err => console.error(err));
+  }
+
+  shouldComponentUpdate() {
+    return true;
   }
 
   render() {
     let { username } = this.props;
-    let { avatarUrl } = this.state;
+    let avatarUrl;
 
-    return (
-      <img
-        alt={username}
-        src={avatarUrl}
-      />
-    );
+    if (this.state && this.state.avatarUrl) {
+      avatarUrl = this.state.avatarUrl;
+    } else {
+      avatarUrl = `https://api.adorable.io/avatars/40/${username}.png`;
+    }
+
+    return <div
+      className="img"
+      style={ {
+        backgroundImage: `url('${avatarUrl}')`,
+      } }
+    />;
   }
 }
